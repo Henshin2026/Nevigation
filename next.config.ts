d8bb14462c0999c85f14baf77253e17552c2cf5b
umi <2412663593@qq.com>
2026-03-24 08:35:53 +0800

    静态导出

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',          // 关键：生成纯静态文件
+  distDir: 'out',            // 输出文件夹叫 out
+  images: {
+    unoptimized: true        // 静态导出必须加这行
+  }
 };
 
 export default nextConfig;
